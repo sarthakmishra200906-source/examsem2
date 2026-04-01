5905c23d0aa68b52c4f6054e4790f2805a4bb055
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-04-01 11:39:42 +0530

    learned to creat a linklist

---
+++ linklist/implink.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+int main(){
+    int choice;
+struct node *head, *newnode, *temp;
+head = 0;
+while (choice){
+    newnode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter your data ");
+    scanf("%d", &newnode->data);
+    newnode->next = 0;
+    if (head == 0)
+    {
+        head = temp = newnode;
+    }
+    else
+    {
+        temp->next = newnode;
+        temp = newnode;
+    }
+    printf("do you want to continu(0,1)?");
+    scanf("%d", &choice);
+}
+temp = head;
+while (temp != 0)
+{
+    printf("%d ",temp->data);
+    temp = temp->next;
+}
+}
